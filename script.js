4ee72bc93014ac9e24a7c96252ca7f2f897a627d
AlexWang624 <55222264+AlexWang624@users.noreply.github.com>
2022-10-03 21:32:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2615,7 +2615,7 @@ const rndFor = randomIntFromInterval(0, 77);
 
 
 function myFunction() {
-  document.getElementById("demo01").innerHTML = "A " + data[0].tarot_interpretations[rndName].name + ", " + " who has the qualities of " + data[0].tarot_interpretations[rndNameQ].keywords + ",";
+  document.getElementById("demo01").innerHTML = "The " + data[0].tarot_interpretations[rndName].name + ", " + " who has the qualities of " + data[0].tarot_interpretations[rndNameQ].keywords + ",";
   document.getElementById("demo02").innerHTML = "and who was " + data[0].tarot_interpretations[rndPast].meanings.light[rndPastMeaning] + ",";
   document.getElementById("demo03").innerHTML = "is now " + data[0].tarot_interpretations[rndPresent].meanings.light[rndPresentMeaning] + ".";
   document.getElementById("demo04").innerHTML = "Blinded by " + data[0].tarot_interpretations[rndHid].keywords[rndHidMeaning] + ",";
